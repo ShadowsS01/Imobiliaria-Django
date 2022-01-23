b98bde97d8cc467ca46ea47a1272b1a8083fe3d7
DKsHs <nicolas.contiero198@gmail.com>
2022-01-22 23:27:54 -0300

    S3 amazon

--- imobi/settings.py
+++ imobi/settings.py
@@ -147,6 +147,10 @@ MESSAGE_TAGS = {
 
 django_on_heroku.settings(locals())
 
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
+AWS_STORAGE_BUCKET_NAME = ''
+
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
